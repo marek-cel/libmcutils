5a2da71a2caf6c03acd98ac5b61b08d52952e973
Marek Cel <marek.cel@gmail.com>
2022-03-29 16:52:18 +0200

    code style checking script

--- run_cpplint.sh
+++ run_cpplint.sh
@@ -4,8 +4,11 @@
 
 rm -f out_cpplint.txt
 
+# --filter=-build/c++11,+build/c++14,-readability/multiline_comment,-readability/alt_tokens,-whitespace/parens,-whitespace/braces,-whitespace/indent,-whitespace/comments,-whitespace/newline,-whitespace/blank_line,-whitespace/comma \
+# --filter=-build/c++11,+build/c++14,-readability/multiline_comment,-readability/alt_tokens,-whitespace/parens,-whitespace/braces,-whitespace/indent,-whitespace/comments,-whitespace/newline,-whitespace/blank_line,-whitespace/comma,-whitespace/line_length,-build/include_order \
+
 cpplint \
-    --filter=-build/c++11,+build/c++14,-readability/multiline_comment,-readability/alt_tokens,-whitespace/parens,-whitespace/braces,-whitespace/indent,-whitespace/comments,-whitespace/newline,-whitespace/blank_line,-whitespace/comma \
+    --filter=-build/c++11,+build/c++14,-readability/multiline_comment,-readability/alt_tokens,-whitespace/parens,-whitespace/braces,-whitespace/indent,-whitespace/comments,-whitespace/newline,-whitespace/blank_line,-whitespace/comma,-whitespace/line_length,-build/include_order \
     --linelength=100 \
     --extensions=h,cpp --root=./ \
     ./mcutils/*.h \
