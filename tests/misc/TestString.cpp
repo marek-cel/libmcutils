7efa5b8b353e6c3cd21ee1d6b89c557699667d7a
Marek Cel <marek.cel@gmail.com>
2022-04-09 09:11:16 +0200

    unit tests - string

--- tests/misc/TestString.cpp
+++ tests/misc/TestString.cpp
@@ -23,7 +23,7 @@ TEST_F(TestString, CanGetFirstLine)
     std::string l3 = "fgh789";
     std::string ml = l1 + "\n" + l2 + "\n" + l3 + "\n";
     std::string res = mc::String::getFirstLine( ml );
-    EXPECT_TRUE( l1 == res );
+    EXPECT_STREQ( l1.c_str(), res.c_str() );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,4 +45,37 @@ TEST_F(TestString, CanICompare)
     EXPECT_FALSE( 0 == mc::String::icompare( lowerCase, upperCase1 ) );
     EXPECT_FALSE( 0 == mc::String::icompare( upperCase, camelCase1 ) );
     EXPECT_FALSE( 0 == mc::String::icompare( lowerCase, camelCase1 ) );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_F(TestString, CanSplitString)
+{
+    std::string s1 = "Lorem ipsum dolor sit amet";
+    std::string s2 = "Lorem_ipsum_dolor_sit_amet";
+
+    std::vector<std::string> vs1 = mc::String::splitString( s1, " " );
+    std::vector<std::string> vs2 = mc::String::splitString( s2, "_" );
+
+    EXPECT_EQ( 5, vs1.size() );
+    EXPECT_EQ( 5, vs2.size() );
+
+    EXPECT_STREQ( vs1[0].c_str(), "Lorem" );
+    EXPECT_STREQ( vs1[1].c_str(), "ipsum" );
+    EXPECT_STREQ( vs1[2].c_str(), "dolor" );
+    EXPECT_STREQ( vs1[3].c_str(), "sit" );
+    EXPECT_STREQ( vs1[4].c_str(), "amet" );
+
+    EXPECT_STREQ( vs2[0].c_str(), "Lorem" );
+    EXPECT_STREQ( vs2[1].c_str(), "ipsum" );
+    EXPECT_STREQ( vs2[2].c_str(), "dolor" );
+    EXPECT_STREQ( vs2[3].c_str(), "sit" );
+    EXPECT_STREQ( vs2[4].c_str(), "amet" );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_F(TestString, CanStripLeadingSpaces)
+{
+
 }
