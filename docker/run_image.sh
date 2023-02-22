caedf7d4c87789859f6181534093791397bc881b
Marek Cel <marek.cel@gmail.com>
2023-02-22 12:38:47 +0100

    Docker building environemnt setup

---
+++ docker/run_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+################################################################################
+
+docker run -it --name my-libmcutils-build-env libmcutils-build-env:1
+
+################################################################################
+
+read -p "Press any key to continue... " -n1 -s
