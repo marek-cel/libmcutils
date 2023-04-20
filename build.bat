14b8bd78fb634fcf35599aaf03fc01895bab849f
Marek Cel <marek.cel@gmail.com>
2023-04-20 21:57:34 +0200

    windows building script style

--- build.bat
+++ build.bat
@@ -3,5 +3,8 @@ call clean.bat
 mkdir build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBMCUTILS_DIR%
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBMCUTILS_DIR%
+
 cmake --build . --config Release
