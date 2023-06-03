5a1e81f386966cd26db81ed5d7a91aa09e3a17eb
Marek Cel <marek.cel@gmail.com>
2023-06-03 14:41:29 +0200

    Set cpp flags when building with tests using script

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ if [ "$1" = "--with-tests" ]; then
     cmake .. \
 	-DTESTING=On \
 	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_FLAGS="-O0 -fno-elide-constructors -fno-default-inline -fprofile-arcs -ftest-coverage" \
 	-DCMAKE_INSTALL_PREFIX=/usr/local
 else
     cmake .. \
