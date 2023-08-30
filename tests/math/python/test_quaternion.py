1e59fb408cc337dcc7e82ee7f531fba051418927
Marek Cel <marek.cel@gmail.com>
2023-08-30 19:33:43 +0200

    Add tests reference scripts in Python

---
+++ tests/math/python/test_quaternion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+################################################################################
+
+from scipy.spatial.transform import Rotation as R
+
+from test_math_utils import get_rotation_matrix
+
+################################################################################
+
