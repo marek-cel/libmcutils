2fa61e20dd5bbad5f88585259e46375932d40b0b
Marek Cel <marek.cel@gmail.com>
2023-11-18 16:12:08 +0100

    Move Dockerfile to misc dir, delete unnecessary scripts

--- docker/run_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-################################################################################
-
-docker run -it --name my-libmcutils-build-env libmcutils-build-env:1
-
-################################################################################
-
-read -p "Press any key to continue... " -n1 -s
