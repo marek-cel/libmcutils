2e71324c7434face3665fc70fafeae4bb341b750
Marek Cel <marek.cel@gmail.com>
2023-12-27 21:35:29 +0100

    Fix code style

--- mcutils/math/Random.cpp
+++ mcutils/math/Random.cpp
@@ -38,9 +38,9 @@ int Random::GetRandom(int min, int max)
     if ( max > min && max <= RAND_MAX )
     {
 #       ifdef _MSC_VER
-        //rand_s(&_rand);
+        rand_s(&rand_);
         return min + rand_ % (max - min + 1);
-        return min + rand() % (max - min + 1);
+        //return min + rand() % (max - min + 1);
 #       else
         mutex_.lock();
         rand_ = rand_r(&seed_);
