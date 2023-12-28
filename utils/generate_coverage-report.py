594bda2e16e7f5962348bddcd006b293189cddc6
Marek Cel <marek.cel@gmail.com>
2023-12-28 09:19:37 +0000

    Rename tools directory as utils directory

--- utils/generate_coverage-report.py
+++ utils/generate_coverage-report.py
@@ -33,7 +33,7 @@ def captureCoverage():
 
 
 def getExclusions():
-    file_path = 'tools/lcov_exclude.txt'
+    file_path = 'utils/lcov_exclude.txt'
     with open(file_path, 'r') as file:
         exclusions = file.readlines()
     exclusions = [line.strip() for line in exclusions]
