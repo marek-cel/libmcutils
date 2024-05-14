4ecb6398a817652798ff832ab4f4e4d39191f614
Marek Cel <marek.cel@gmail.com>
2024-05-14 11:51:31 +0000

    Add tests for converting right ascension declination to azimuth elevation

--- tests/astro/TestRaDec2AzEl.cpp
+++ tests/astro/TestRaDec2AzEl.cpp
@@ -16,5 +16,20 @@ protected:
 
 TEST_F(TestRaDec2AzEl, CanComputeAzElFromRaDecLatAndLST1)
 {
-    // TODO
+    // expected values calculated with PyEphem
+    // tests/astro/python/test_radec2azel.py
+
+    mc::AzEl azel;
+    mc::RaDec radec;
+
+    radec.ra = 5.623280496934751;
+    radec.dec = -0.2513744984225895;
+
+    double lat = mc::Units::deg2rad(52.2296756);
+    double lst = 5.956121595178459;
+
+    azel = mc::RaDec2AzEl(radec, lat, lst);
+
+    EXPECT_NEAR(azel.az, 3.4882750511169434, 1.0e-3);
+    EXPECT_NEAR(azel.el, 0.3733336627483368, 1.0e-3);
 }
--- tests/astro/python/test_radec2azel.py
+++ tests/astro/python/test_radec2azel.py
@@ -4,50 +4,28 @@ import datetime
 import ephem
 import math
 
-# lat_deg = 52.3033333
-# lon_deg = 21.1605556
-# lat = math.radians(lat_deg)
-# lon = math.radians(lon_deg)
-# alt = 100.0
-
-# print("Lat: " + str(lat) + " Lon: " + str(lon))
-
-# observer = ephem.Observer()
-# observer.lon = lon
-# observer.lat = lat
-# observer.elevation = alt
-
 year   = 2000
 month  = 1
 day    = 1
-hour   = 0
+hour   = 12
 minute = 0
 second = 0
 dt = datetime.datetime(year, month ,day, hour, minute, second)
-# jd = ephem.julian_date(dt)
-# observer.date = jd
+jd = ephem.julian_date(dt)
 
-# sun = ephem.Sun()
-# sun.compute(observer)
+warsaw = ephem.city("Warsaw")
+warsaw.date = jd
 
+lon = math.degrees(warsaw.lon)
+lat = math.degrees(warsaw.lat)
 
+sun = ephem.Sun()
+sun.compute(warsaw)
 
-lon = ephem.degrees('21.1605556')
-lat = ephem.degrees('52.3033333')
-
-star = ephem.FixedBody()
-star._ra = math.pi
-star._dec = 0.0
-
-observer = ephem.Observer()
-observer.date = 0
-observer.lon = 0.0
-observer.lat = 0.0
-
-star.compute(observer)
-
-# print("LST: ", float(observer.sidereal_time()))
-print("RA: ", float(star.ra))
-print("dec: ", float(star.dec))
-print("az: ", float(star.az))
-print("el: ", float(star.alt))
+print("LST: ", float(warsaw.sidereal_time()))
+print("lat: ", float(lat))
+print("lon: ", float(lon))
+print("RA: ", float(sun.ra))
+print("dec: ", float(sun.dec))
+print("az: ", float(sun.az))
+print("el: ", float(sun.alt))
