9eccafe08a9db75b91e765b5791abaf0160686df
Marek Cel <marek.cel@gmail.com>
2024-12-05 17:42:28 +0000

    Remove JulianDate.cpp and update CMakeLists.txt

--- mcutils/astro/JulianDate.h
+++ mcutils/astro/JulianDate.h
@@ -22,7 +22,6 @@
 #ifndef MCUTILS_ASTRO_JULIANDATE_H_
 #define MCUTILS_ASTRO_JULIANDATE_H_
 
-#include <mcutils/defs.h>
 #include <mcutils/time/DateTime.h>
 
 namespace mc {
@@ -34,22 +33,65 @@ namespace mc {
  * - Meeus J.: Astronomical Algorithms, 1998
  * - [Julian day - Wikipedia](https://en.wikipedia.org/wiki/Julian_day)
  */
-class MCUTILSAPI JulianDate
+class JulianDate
 {
 public:
 
-    static double GetJulianDate(const DateTime& gd);
+    static double GetJulianDate(const DateTime& gd)
+    {
+        double jd = 0.0;
+
+        // Meeus J.: Astronomical Algorithms, p.61
+        double y = gd.year;
+        double m = gd.month;
+        double d = gd.day;
+
+        if (m == 1 || m == 2)
+        {
+            y = y - 1;
+            m = 12 + m;
+        }
+
+        double a = static_cast<int>(y * 0.01);
+        double b = 2.0 - a + static_cast<int>(a * 0.25);
+
+        jd = static_cast<int>(365.25 * (y + 4716.0))
+        + static_cast<int>(30.6001 * (m + 1.0))
+        + d + b - 1524.5;
+
+        jd += gd.hour   / 24.0;
+        jd += gd.minute / (24.0 * 60.0);
+        jd += gd.second / (24.0 * 60.0 * 60.0);
+
+        return jd;
+    }
 
     /**
      * \param gd Gregorian date UTC
     */
-    explicit JulianDate(const DateTime& gd = DateTime());
+    explicit JulianDate(const DateTime& gd = DateTime())
+    {
+        SetFromGregorianDate(gd);
+    }
 
     /**
      * \brief Sets Julian date from Gregorian date.
      * \param dateTime Gregorian date UTC
     */
-    void SetFromGregorianDate(const DateTime& gd);
+    void SetFromGregorianDate(const DateTime& gd)
+    {
+        _gd = gd;
+
+        if (_gd.month  <   1) _gd.month  = 1;
+        if (_gd.month  >  12) _gd.month  = 12;
+        if (_gd.day    >  31) _gd.day    = 31;
+        if (_gd.hour   >  23) _gd.hour   = 23;
+        if (_gd.minute >  59) _gd.minute = 59;
+        if (_gd.second >  59) _gd.second = 59;
+        if (_gd.msec   > 999) _gd.msec   = 999;
+
+        Update();
+    }
 
     inline double jc() const { return _jc; }
     inline double jd() const { return _jd; }
@@ -61,7 +103,13 @@ private:
     double _jc = 0.0;   ///< Julian century
 
 
-    void Update();
+    void Update()
+    {
+        _jd = GetJulianDate(_gd);
+
+        // Meeus J.: Astronomical Algorithms, p.87
+        _jc = (_jd - 2451545.0) / 36525.0;
+    }
 };
 
 } // namespace mc
