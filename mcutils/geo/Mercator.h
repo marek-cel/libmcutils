f97b00e7feff495b67e914d35bf5ecae7dbeae0e
Marek Cel <marek.cel@gmail.com>
2024-12-05 17:27:59 +0000

    Refactor geo module: remove Mercator.cpp and update CMakeLists.txt

--- mcutils/geo/Mercator.h
+++ mcutils/geo/Mercator.h
@@ -22,9 +22,9 @@
 #ifndef MCUTILS_GEO_MERCATOR_H_
 #define MCUTILS_GEO_MERCATOR_H_
 
-#include <mcutils/defs.h>
-
 #include <mcutils/geo/Ellipsoid.h>
+
+#include <mcutils/misc/Units.h>
 
 namespace mc {
 
@@ -36,7 +36,7 @@ namespace mc {
  * - Grafarend E., et al.: Map Projections: Carthographic Information Systems, 2006, p.490
  * - Deetz C., Adams O.: Elements of Map Projection, US Coast and Geodetic Survery, 1931, p.101
  */
-class MCUTILSAPI Mercator
+class Mercator
 {
 public:
 
@@ -44,7 +44,11 @@ public:
      * \brief Constructor
      * \param e datum ellipsoid
      */
-    Mercator(const Ellipsoid& e);
+    Mercator(const Ellipsoid& e)
+        : _e(e)
+        , _max_x(CalculateX(Units::deg2rad( 180.0 )))
+        , _max_y(CalculateY(Units::deg2rad(  85.0 )))
+    {}
 
     /**
      * \brief Calculates geodetic latitude.
@@ -54,35 +58,55 @@ public:
      * \return geodetic latitude [rad]
      */
     double CalculateLat(double y, double max_error = 1.0e-9,
-                        unsigned int max_iterations = 10);
+                        unsigned int max_iterations = 10)
+    {
+        // for lat_ts=0 k0=a
+        return CalculateT_inv(exp(-y / _e.a()), max_error, max_iterations);
+    }
 
     /**
      * \brief Calculates geodetic longitude.
      * \param x [m] Mercator x-coordinate
      * \return geodetic longitude [rad]
      */
-    double CalculateLon(double x);
+    double CalculateLon(double x)
+    {
+        // for lat_ts=0 k0=a
+        return x / _e.a();
+    }
 
     /**
      * \brief Calculates Mercator x-coordinate.
      * \param lon [rad] geodetic longitude
      * \return Mercator x-coordinate [m]
      */
-    double CalculateX(double lon);
+    double CalculateX(double lon)
+    {
+        // for lat_ts=0 k0=a
+        return _e.a() * lon;
+    }
 
     /**
      * \brief Calculates Mercator y-coordinate.
      * \param lat [rad] geodetic latitude
      * \return Mercator y-coordinate [m]
      */
-    double CalculateY(double lat);
+    double CalculateY(double lat)
+    {
+        // for lat_ts=0 k0=a
+        return _e.a() * log(CalculateT(lat));
+    }
 
     /**
      * \brief Calculates Isometric Latitude kernel.
      * \param lat [rad] geodetic latitude
      * \return Isometric Latitude kernel
      */
-    double CalculateT(double lat);
+    double CalculateT(double lat)
+    {
+        double e_sinLat = _e.e() * sin(lat);
+        return tan(M_PI_4 + 0.5 * lat) * pow((1.0 - e_sinLat) / (1.0 + e_sinLat), 0.5 * _e.e());
+    }
 
     /**
      * \brief Calculates geodetic latitude from the isometric latitude.
@@ -92,7 +116,28 @@ public:
      * \return geodetic latitude [rad]
      */
     double CalculateT_inv(double t, double max_error = 1.0e-9,
-                          unsigned int max_iterations = 10);
+                          unsigned int max_iterations = 10)
+    {
+        double lat = M_PI_2 - 2.0 * atan(t);
+        double ex = 0.5 * _e.e();
+        double er = 1.0e16;
+
+        unsigned int iteration = 0;
+
+        while ( er > max_error && iteration < max_iterations )
+        {
+            double e_sinLat = _e.e() * sin(lat);
+            double lat_new = M_PI_2
+                - 2.0 * atan(t * pow((1.0 - e_sinLat) / (1.0 + e_sinLat), ex));
+
+            er = fabs(lat_new - lat);
+            lat = lat_new;
+
+            iteration++;
+        }
+
+        return lat;
+    }
 
     double max_x() const { return _max_x; }
     double max_y() const { return _max_y; }
