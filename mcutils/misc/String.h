8b427a66df64d755baef4871234af25e41e44d0c
Marek Cel <marek.cel@gmail.com>
2024-12-06 11:20:35 +0100

    Remove String.cpp and update CMakeLists.txt to reflect the changes

--- mcutils/misc/String.h
+++ mcutils/misc/String.h
@@ -22,11 +22,14 @@
 #ifndef MCUTILS_MISC_STRING_H_
 #define MCUTILS_MISC_STRING_H_
 
+#include <cstdio>
+#include <sstream>
+#include <cstring>
 #include <limits>
 #include <string>
 #include <vector>
 
-#include <mcutils/defs.h>
+#include <mcutils/misc/Check.h>
 
 namespace mc {
 namespace String {
@@ -36,7 +39,225 @@ namespace String {
  * \param str string to be processed
  * \return string first line
  */
-MCUTILSAPI std::string GetFirstLine(const std::string& str);
+inline std::string GetFirstLine(const std::string& str)
+{
+    std::istringstream iss(str);
+    std::string firstLine;
+    std::getline(iss, firstLine);
+    return firstLine;
+}
+
+/**
+ * \brief Splits string with the given delimeter.
+ * \param str string to be splitted
+ * \param sep string separator
+ * \return vector of strings
+ */
+inline std::vector<std::string> SplitString(const std::string& str, const std::string& sep)
+{
+    std::vector<std::string> result;
+
+    std::string temp = str;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = temp.find(sep)) != std::string::npos)
+    {
+        token = temp.substr(0, pos);
+        result.push_back(token);
+        temp.erase(0, pos + 1);
+    }
+    result.push_back(temp);
+    return result;
+}
+
+/**
+ * \brief Returns string with leading white spaces removed.
+ * \param str string to be processed
+ * \return string with leading white spaces removed
+ */
+inline std::string StripLeadingSpaces(const std::string& str)
+{
+    std::string::size_type offset = 0;
+    for (unsigned int i = 0; i < str.size(); ++i)
+    {
+        if (!isspace(str.c_str()[i]))
+        {
+            break;
+        }
+        offset++;
+    }
+    return str.substr(offset);
+}
+
+/**
+ * \brief Returns string with trailing white spaces removed.
+ * \param str string to be processed
+ * \return string with trailing white spaces removed
+ */
+inline std::string StripTrailingSpaces(const std::string& str)
+{
+    std::string::size_type offset = 0;
+    for (unsigned int i = 0; i < str.size(); ++i)
+    {
+        if (!isspace(str.c_str()[i]))
+        {
+            offset = static_cast<std::string::size_type>(i);
+        }
+    }
+    return str.substr(0, offset + 1);
+}
+
+/**
+ * \brief Returns string with both leading and trailing white spaces removed.
+ * \param str string to be processed
+ * \return string with leading and trailing white spaces removed
+ */
+inline std::string StripSpaces(const std::string& str)
+{
+    std::string::size_type offset_l = 0;
+    std::string::size_type offset_t = 0;
+    bool l_passed = false;
+    for (unsigned int i = 0; i < str.size(); ++i)
+    {
+        if (!isspace(str.c_str()[i]))
+        {
+            offset_t = i;
+            l_passed = true;
+        }
+        else
+        {
+            if (!l_passed) offset_l++;
+        }
+    }
+    return str.substr(offset_l, offset_t - offset_l + 1);
+}
+
+/**
+ * \brief Converts string into variable.
+ * \param str string to be processed
+ * \param def default value
+ * \return result value
+ */
+inline bool ToBool(const std::string& str, bool def = std::numeric_limits<bool>::quiet_NaN())
+{
+    bool result = def;
+    int temp = std::numeric_limits<int>::quiet_NaN();
+    std::stringstream ss(str);
+    ss >> temp;
+    if (IsValid(temp))
+    {
+        result = temp != 0;
+    }
+    return result;
+}
+
+/**
+ * \brief Converts string into variable.
+ * \param str string to be processed
+ * \param def default value
+ * \return result value
+ */
+inline int ToInt(const std::string& str, int def = std::numeric_limits<int>::quiet_NaN())
+{
+    int result = def;
+    int temp = std::numeric_limits<int>::quiet_NaN();
+    std::stringstream ss(str);
+    ss >> temp;
+    if (IsValid(temp))
+    {
+        result = temp;
+    }
+    return result;
+}
+
+/**
+ * \brief Converts string into variable.
+ * \param str string to be processed
+ * \param def default value
+ * \return result value
+ */
+inline double ToDouble(const std::string& str, double def = std::numeric_limits<double>::quiet_NaN())
+{
+    double result = def;
+    double temp = std::numeric_limits<double>::quiet_NaN();
+    std::stringstream ss(str);
+    ss >> temp;
+    if (IsValid(temp))
+    {
+        result = temp;
+    }
+    return result;
+}
+
+/**
+ * \brief Converts variable into string.
+ * \param val value to be processed
+ * \return result string
+ */
+inline std::string ToString(int val)
+{
+    std::stringstream ss;
+    ss << val;
+    return ss.str();
+}
+
+/**
+ * \brief Converts variable into string.
+ * \param val value to be processed
+ * \return result string
+ */
+inline std::string ToString(float val)
+{
+    std::stringstream ss;
+    ss << val;
+    return ss.str();
+}
+
+/**
+ * \brief Converts variable into string.
+ * \param val value to be processed
+ * \return result string
+ */
+inline std::string ToString(double val)
+{
+    std::stringstream ss;
+    ss << val;
+    return ss.str();
+}
+
+/**
+ * \brief Converts string to lower case.
+ * \param str string to be processed
+ * \return result string
+ */
+inline std::string ToLower(const std::string& str)
+{
+    std::string result;
+    result.clear();
+    unsigned int length = static_cast<unsigned int>(str.length());
+    for (unsigned int i = 0; i < length; ++i)
+    {
+        result += static_cast<char>( tolower(str.c_str()[i]) );
+    }
+    return result;
+}
+
+/**
+ * \brief Converts string to upper case.
+ * \param str string to be processed
+ * \return result string
+ */
+inline std::string ToUpper(const std::string& str)
+{
+    std::string result;
+    result.clear();
+    unsigned int length = static_cast<unsigned int>(str.length());
+    for (unsigned int i = 0; i < length; ++i)
+    {
+        result += static_cast<char>( toupper(str.c_str()[i]) );
+    }
+    return result;
+}
 
 /**
  * \brief Compares strings.
@@ -45,101 +266,17 @@ MCUTILSAPI std::string GetFirstLine(const std::string& str);
  * \param case_sensitive specifies if comparison is case sensitive
  * \return integer result of the comparison (returns 0 if strings are equal)
  */
-MCUTILSAPI int Compare(const std::string& str_1,
-                       const std::string& str_2,
-                       bool case_sensitive = false);
-
-/**
- * \brief Splits string with the given delimeter.
- * \param str string to be splitted
- * \param sep string separator
- * \return vector of strings
- */
-MCUTILSAPI std::vector<std::string> SplitString(const std::string& str,
-                                                const std::string& sep);
-
-/**
- * \brief Returns string with leading white spaces removed.
- * \param str string to be processed
- * \return string with leading white spaces removed
- */
-MCUTILSAPI std::string StripLeadingSpaces(const std::string& str);
-
-/**
- * \brief Returns string with trailing white spaces removed.
- * \param str string to be processed
- * \return string with trailing white spaces removed
- */
-MCUTILSAPI std::string StripTrailingSpaces(const std::string& str);
-
-/**
- * \brief Returns string with both leading and trailing white spaces removed.
- * \param str string to be processed
- * \return string with leading and trailing white spaces removed
- */
-MCUTILSAPI std::string StripSpaces(const std::string& str);
-
-/**
- * \brief Converts string into variable.
- * \param str string to be processed
- * \param def default value
- * \return result value
- */
-MCUTILSAPI bool ToBool(const std::string& str,
-                       bool def = std::numeric_limits<bool>::quiet_NaN());
-
-/**
- * \brief Converts string into variable.
- * \param str string to be processed
- * \param def default value
- * \return result value
- */
-MCUTILSAPI int ToInt(const std::string& str,
-                     int def = std::numeric_limits<int>::quiet_NaN());
-
-/**
- * \brief Converts string into variable.
- * \param str string to be processed
- * \param def default value
- * \return result value
- */
-MCUTILSAPI double ToDouble(const std::string& str,
-                           double def = std::numeric_limits<double>::quiet_NaN());
-
-/**
- * \brief Converts variable into string.
- * \param val value to be processed
- * \return result string
- */
-MCUTILSAPI std::string ToString(int val);
-
-/**
- * \brief Converts variable into string.
- * \param val value to be processed
- * \return result string
- */
-MCUTILSAPI std::string ToString(float val);
-
-/**
- * \brief Converts variable into string.
- * \param val value to be processed
- * \return result string
- */
-MCUTILSAPI std::string ToString(double val);
-
-/**
- * \brief Converts string to lower case.
- * \param str string to be processed
- * \return result string
- */
-MCUTILSAPI std::string ToLower(const std::string& str);
-
-/**
- * \brief Converts string to upper case.
- * \param str string to be processed
- * \return result string
- */
-MCUTILSAPI std::string ToUpper(const std::string& str);
+inline int Compare(const std::string& str_1, const std::string& str_2, bool case_sensitive = false)
+{
+    std::string strTemp_1 = str_1;
+    std::string strTemp_2 = str_2;
+    if (!case_sensitive)
+    {
+        strTemp_1 = String::ToLower(str_1);
+        strTemp_2 = String::ToLower(str_2);
+    }
+    return strTemp_1.compare(strTemp_2);
+}
 
 } // namespace String
 } // namespace mc
